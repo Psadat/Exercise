b8d8be1b3d0d84bc567e4db8e802e7feb0f5f996
eloyjoon <alireza.sabouei@gmail.com>
2021-08-31 12:20:23 +0430

    Exercise01

--- Max of Two Numbers.js
+++ Max of Two Numbers.js
@@ -1,1 +1,13 @@
-console.log('Hello')
+let a ;
+let b ;
+
+function maximumfunction(a , b){
+    if ( a > b ){
+        console.log(a);
+    }
+    else{
+        console.log(b);
+    }
+}
+
+maximumfunction(10,15);
